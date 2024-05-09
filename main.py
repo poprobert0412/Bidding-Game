258ac7aa425ad592f43c6d99c7d6f6592b7eb6cd
Robert <poprobert1999@yahoo.com>
2024-05-09 17:54:05 +0300

    Final project

--- main.py
+++ main.py
@@ -9,20 +9,25 @@ def clear_console():
     else:
         _ = os.system('clear')
 
-
-auction_dict = {
-    "name": [],
-    "bid": []
-}
-
-while True:
-    print(logo)
-    print("Welcome to the secret autction program.")
-    name = input("What is your name?: ")
-    bid = float(input("What's your bid?: $"))
-    auction_dict["name"] = name
-    auction_dict["bid"] = bid
-    another_bidder = input("are there any other bidders? Type 'yes' or 'no': ").lower()
+def bidding_game():
+    auction_dict = {
+    }
     while True:
-        if another_bidder == 'yes':
-
+        print(logo)
+        print("Welcome to the secret autction program.")
+        name = input("What is your name?: ")
+        bid = int(input("What's your bid?: $"))
+        auction_dict[name] = bid
+        another_bidder = input("Are there any other bidders? Type 'yes' or 'no': ").lower()
+        while True:
+            if another_bidder == 'yes':
+                clear_console()
+                break
+            elif another_bidder == "no":
+                max_value = max(auction_dict.values())
+                max_key = max(auction_dict, key=auction_dict.get)
+                print(f"The winner is {max_key} with a bid of ${max_value}")
+                return
+            else:
+                print("Wrong input! Please type 'yes' or 'no'")
+bidding_game()
